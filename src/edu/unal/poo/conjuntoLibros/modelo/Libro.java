2c0946e88cacd6b940d93e4cfd346ade08e2764e
Estudiante <Estudiante@10.203.2.245>
2018-02-27 08:09:33 -0500

    primera versión prueba

---
+++ src/edu/unal/poo/conjuntoLibros/modelo/Libro.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.unal.poo.conjuntoLibros.modelo;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Libro {
+    private String titulo;
+    private int numeroPaginas;
+    private int calificacion;
+    
+    //Asociacion: Cardinalidd 0..1 o 1
+    private Autor autor;
+
+    public Libro(String titulo, int numeroPaginas) {
+        this.titulo = titulo;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+  
+}
